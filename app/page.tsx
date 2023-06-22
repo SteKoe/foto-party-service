f77c7970fd6cfe46d2bf0b10fe23ed8ce7988cbc
Stephan Köninger <github@stekoe.de>
2023-06-22 20:20:03 +0200

    chore: mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between px-4 py-8 md:p-24">
             <Image src="/img/hero.jpg" width={1024} height={256} alt="" className="rounded-2xl mx-auto"/>
-            <div className="w-8/12 p-5 mx-auto">
-            </div>
         </main>
     )
 }
