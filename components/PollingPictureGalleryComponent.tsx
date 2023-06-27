146b77f031649434279c3960072ec36e59bcb7d8
Stephan Köninger <github@stekoe.de>
2023-06-27 17:42:37 +0200

    chore: mobile

--- components/PollingPictureGalleryComponent.tsx
+++ components/PollingPictureGalleryComponent.tsx
@@ -3,7 +3,7 @@
 import React, {useEffect, useState} from "react";
 import {PictureGalleryComponent} from "@/components/PictureGalleryComponent";
 import ToggleFullscreenButton from "@/components/ToggleFullscreen";
-import {usePathname, useSearchParams} from "next/navigation";
+import {useSearchParams} from "next/navigation";
 
 const POLLING_INTERVAL = 60;
 
@@ -12,9 +12,9 @@ export default function PollingPictureGalleryComponent() {
     const [countDown, setCountDown] = useState(POLLING_INTERVAL);
     const [isLoading, setIsLoading] = useState(false);
     const searchParams = useSearchParams();
-    
+
     const galleryColumns = Number(searchParams.get("columns") ?? 4);
-    
+
     async function getData() {
         const res = await fetch(`/api/pictures?count=${galleryColumns * 10}`)
         if (!res.ok) {
@@ -22,9 +22,9 @@ export default function PollingPictureGalleryComponent() {
         }
         return res.json()
     }
-    
+
     useEffect(() => {
-        const  initialLoad = async () => {
+        const initialLoad = async () => {
             setIsLoading(true);
             let data = await getData();
             setImages(data);
@@ -59,9 +59,9 @@ export default function PollingPictureGalleryComponent() {
         <>
             <div className={"fixed top-1 right-1 flex gap-1 text-black"}>
                 <div
-                    className={"bg-white bg-opacity-50 text-center h-10 w-10 rounded flex justify-center items-center "}>
+                    className={"bg-white bg-opacity-50 text-center h-7 w-7 text-xs md:h-10 md:w-10 md:text-base rounded flex justify-center items-center "}>
                     {isLoading ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" className={"animate-spin"} height="1em"
+                        <svg xmlns="http://www.w3.org/2000/svg" className={"animate-spin m-2"}
                              viewBox="0 0 512 512">
                             <path
                                 d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/>
--- components/ToggleFullscreen.tsx
+++ components/ToggleFullscreen.tsx
@@ -41,14 +41,14 @@ export default function ToggleFullscreenButton() {
 
     return (
         <button onClick={onClick}
-                className={"bg-white bg-opacity-50 aspect-square px-3 rounded"}>
+                className={"bg-white bg-opacity-50 h-7 w-7 text-xs md:h-10 md:w-10 md:text-base rounded"}>
             {isFullscreen ? (
-                <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" height=".9em" viewBox="0 0 512 512">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" viewBox="0 0 512 512" className={"m-2 md:m-3"}>
                     <path
                         d="M439 7c9.4-9.4 24.6-9.4 33.9 0l32 32c9.4 9.4 9.4 24.6 0 33.9l-87 87 39 39c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8H296c-13.3 0-24-10.7-24-24V72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2l39 39L439 7zM72 272H216c13.3 0 24 10.7 24 24V440c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-39-39L73 505c-9.4 9.4-24.6 9.4-33.9 0L7 473c-9.4-9.4-9.4-24.6 0-33.9l87-87L55 313c-6.9-6.9-8.9-17.2-5.2-26.2s12.5-14.8 22.2-14.8z"/>
                 </svg>
             ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" height=".9em" viewBox="0 0 512 512">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" viewBox="0 0 512 512" className={"m-2 md:m-3"}>
                     <path
                         d="M344 0H488c13.3 0 24 10.7 24 24V168c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-39-39-87 87c-9.4 9.4-24.6 9.4-33.9 0l-32-32c-9.4-9.4-9.4-24.6 0-33.9l87-87L327 41c-6.9-6.9-8.9-17.2-5.2-26.2S334.3 0 344 0zM168 512H24c-13.3 0-24-10.7-24-24V344c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2l39 39 87-87c9.4-9.4 24.6-9.4 33.9 0l32 32c9.4 9.4 9.4 24.6 0 33.9l-87 87 39 39c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8z"/>
                 </svg>
