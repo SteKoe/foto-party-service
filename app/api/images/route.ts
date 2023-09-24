0b9f2074615e68bfd803da76f733892674687f39
Stephan Köninger <github@stekoe.de>
2023-09-24 22:28:38 +0200

    fix: build

--- app/api/images/route.ts
+++
@@ -1,7 +0,0 @@
-import {NextRequest, NextResponse} from "next/server";
-import {readToken} from "@/middleware";
-import listFiles from "@/app/api/GoogleDriveClient";
-
-export async function GET(request: NextRequest) {
-    return NextResponse.json(await listFiles())
-}
