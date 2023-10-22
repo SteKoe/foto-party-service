bdd63b1d7ff776f2425774e866ecfbe1c9047cd7
Stephan Köninger <github@stekoe.de>
2023-10-22 13:50:55 +0200

    fix: 🔧 database access on vercel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
+const {PrismaPlugin} = require("@prisma/nextjs-monorepo-workaround-plugin");
+
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: false,
     experimental: {
         serverActions: true
-    }
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()]
+        }
+
+        return config
+    },
 };
 
 module.exports = nextConfig;
