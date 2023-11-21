501c589a661362b3269a0211bbeb53ff13c30606
Stephan Köninger <github@stekoe.de>
2023-11-21 18:58:46 +0100

    fix(leerzeichen): fix Leerzeichen

--- components/form/Field.tsx
+++ components/form/Field.tsx
@@ -1,34 +1,41 @@
 'use client';
 
-import {BooleanField} from "@/components/form/BooleanField";
-import {InvitationGuestOption} from "@/prisma/generated/client";
-import {Textarea} from "@nextui-org/input";
+import { BooleanField } from '@/components/form/BooleanField';
+import { InvitationGuestOption } from '@/prisma/generated/client';
+import { Textarea } from '@nextui-org/input';
 
 type FieldProps = {
-    option: InvitationGuestOption,
-    guestId: string,
-    value: any,
-    onChange: (e: InvitationGuestOption & { value: any }) => void
-}
+    option: InvitationGuestOption;
+    guestId: string;
+    value: any;
+    onChange: (e: InvitationGuestOption & { value: any }) => void;
+};
 
 export function Field(props: FieldProps) {
     switch (props.option.type) {
         case 'boolean':
-            return <BooleanField guestId={props.guestId}
-                                 fieldDefinition={props.option}
-                                 value={props.value}
-                                 onChange={props.onChange}
-            />
+            return (
+                <BooleanField
+                    guestId={props.guestId}
+                    fieldDefinition={props.option}
+                    value={props.value}
+                    onChange={props.onChange}
+                />
+            );
         case 'text':
-            return <Textarea
-                label={props.option.name}
-                color="secondary"
-                className="col-span-12"
-                value={props.value ?? ''}
-                onValueChange={(value) => props.onChange({
-                    ...props.option,
-                    value: value.trim()
-                })}
-            ></Textarea>
+            return (
+                <Textarea
+                    label={props.option.name}
+                    color="secondary"
+                    className="col-span-12"
+                    value={props.value ?? ''}
+                    onValueChange={(value) => {
+                        props.onChange({
+                            ...props.option,
+                            value: value,
+                        });
+                    }}
+                ></Textarea>
+            );
     }
 }
