7c0a97a11d29c7ab2ddd62f4487ea75fef5290a7
Stephan Köninger <github@stekoe.de>
2024-07-13 21:14:24 +0200

    fix: AWS_ACCESS_KEY is reserved on vercel

--- app/api/S3Client.ts
+++ app/api/S3Client.ts
@@ -10,7 +10,7 @@ const client = new S3Client({
   apiVersion: "2006-03-01",
   region: process.env.AWS_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    accessKeyId: process.env.AWS__ACCESS_KEY!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
