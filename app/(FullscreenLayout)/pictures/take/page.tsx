cc29b2fb05f26923bfa2e80c53ed3e3b66a35aab
Stephan Köninger <github@stekoe.de>
2024-10-17 07:30:17 +0200

    chore: preparing

--- app/(FullscreenLayout)/pictures/take/page.tsx
+++ app/(FullscreenLayout)/pictures/take/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import PicturesPageComponent from "@/components/PicturesPageComponent";
 
 export const metadata: Metadata = {
-  title: "Katharina & Kevin | Mache ein Foto und teiles es!",
+  title: "Classic Tanzparty | Mache ein Foto und teiles es!",
 };
 
 export default function Page() {
